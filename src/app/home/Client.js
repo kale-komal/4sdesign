7050fdfe62dc56825ec22718fa1b89c04d017ec6
kale-komal <komalkale3013@gmail.com>
2024-12-03 16:08:03 -0800

    4s design website

---
+++ src/app/home/Client.js
@@ -0,0 +1,76 @@
+const Client = () => {
+    return (
+      <section id="client">
+        <div className="container">
+          <div className="row mt-3 d-flex justify-content-evenly">
+            <h1 className="heading text-center mb-4 mt-3">CLIENTS</h1>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-1.jpg" alt="" />
+                <p className="logoname">Saroj Group of Companies</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-2.jpg" alt="" />
+                <p className="logoname">Mahindra</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-3.jpg" alt="" />
+                <p className="logoname">JSW</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-4.jpg" alt="" />
+                <p className="logoname">Kirloskar</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-5.jpg" alt="" />
+                <p className="logoname"> Teknic</p>
+              </div>
+            </div>
+          </div>
+  
+          <div className="row mt-2 d-flex justify-content-evenly">
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-6.jpg" alt="" />
+                <p className="logoname">Landcraft Agro</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-7.jpg" alt="" />
+                <p className="logoname">BMR Group</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-8.jpg" alt="" />
+                <p className="logoname">GKB Ophthalmics Ltd</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-9.jpg" alt="" />
+                <p className="logoname">Sevasadan Life Line</p>
+              </div>
+            </div>
+            <div className="col-6 col-lg-2">
+              <div className="image-wrapper">
+                <img src="/images/client/logo-10.jpg" alt="" />
+                <p className="logoname">Teknorix</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  };
+  
+  export default Client
